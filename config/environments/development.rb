c2b6661cb853f8bae4fd7a2dd9b7817c8eb7f09e
Paul Tashman <paul.tashman@careerbuilder.com>
2015-06-13 19:40:22 -0400

    Updates dev config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.action_dispatch.tld_length = 0
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
