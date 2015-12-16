ded01c1ad6eb77f4172309572683381565575c76
Paul Tashman <paul.tashman@pgi.com>
2015-12-16 18:15:17 -0500

    Fixes glitches in site user and trade item creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928230027) do
+ActiveRecord::Schema.define(version: 20151216231345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(version: 20150928230027) do
     t.integer  "site_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
